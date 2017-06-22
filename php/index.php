5a8038aa8bcaf56291a10f05b49d4c919b03cc5c
root <root@fsociety-Mr.Robot>
2017-06-22 09:44:49 +0800

    Sample Chart Added on Dashboard

--- php/index.php
+++ php/index.php
@@ -198,7 +198,8 @@ include 'session.php';
             </div>
         </div>
         <div class="row">
-
+        <!-- ChartJS -->
+            <canvas id="myChart" width="400" height="400"></canvas>
         </div>
         <!-- /.row -->
         <!-- /.container-fluid -->
@@ -208,5 +209,46 @@ include 'session.php';
 <script type="text/javascript" src="../js/jquery.min.js"></script>
 <script type="text/javascript" src="../js/script.js"></script>
 <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/Chart.js">
+$(function() {
+var ctx = $("#myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+});
+</script>
 </body>
 </html>
