748306f3f000f018835615f9edd9af885b7dc21a
root <root@fsociety-Mr.Robot>
2017-07-01 16:37:42 +0800

    Added CSV Import

--- php/settings.php
+++ php/settings.php
@@ -1,5 +1,7 @@
 <?php
 include 'session.php';
+include 'crud.php';
+$oop = new CRUD();
 ?>
 <!DOCTYPE html>
 <html>
@@ -101,60 +103,6 @@ include 'session.php';
         </div>
         <!-- /.navbar-collapse -->
     </nav>
-<?php     
-    // EXAMPLE: IMPORT_TABLES("localhost","user","pass","db_name", "my_baseeee.sql"); //TABLES WILL BE OVERWRITTEN
-                // P.S. IMPORTANT NOTE for people who try to change/replace some strings  in SQL FILE before importing, MUST READ:  https://goo.gl/2fZDQL
-// https://github.com/tazotodua/useful-php-scripts 
-function IMPORT_TABLES($host,$user,$pass,$dbname, $sql_file_OR_content){
-    set_time_limit(3000);
-    $SQL_CONTENT = (strlen($sql_file_OR_content) > 300 ?  $sql_file_OR_content : file_get_contents($sql_file_OR_content)  );  
-    $allLines = explode("\n",$SQL_CONTENT); 
-    $mysqli = new mysqli($host, $user, $pass, $dbname); if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();} 
-        $zzzzzz = $mysqli->query('SET foreign_key_checks = 0');         preg_match_all("/\nCREATE TABLE(.*?)\`(.*?)\`/si", "\n". $SQL_CONTENT, $target_tables); foreach ($target_tables[2] as $table){$mysqli->query('DROP TABLE IF EXISTS '.$table);}         $zzzzzz = $mysqli->query('SET foreign_key_checks = 1');    $mysqli->query("SET NAMES 'utf8'");   
-    $templine = ''; // Temporary variable, used to store current query
-    foreach ($allLines as $line)    {                                           // Loop through each line
-        if (substr($line, 0, 2) != '--' && $line != '') {$templine .= $line;    // (if it is not a comment..) Add this line to the current segment
-            if (substr(trim($line), -1, 1) == ';') {        // If it has a semicolon at the end, it's the end of the query
-                if(!$mysqli->query($templine)){ print('Error performing query \'<strong>' . $templine . '\': ' . $mysqli->error . '<br /><br />');  }  $templine = ''; // set variable to empty, to start picking up the lines after ";"
-            }
-        }
-    }   return 'Importing finished. Now, Delete the import file.';
-}   //see also export.php 
-?>
-<?php
-// EXAMPLE:   EXPORT_TABLES("localhost","user","pass","db_name" ); 
-        //optional: 5th parameter - to backup specific tables only: array("mytable1","mytable2",...)   
-        //optional: 6th parameter - backup filename
-        // IMPORTANT NOTE for people who try to change strings in SQL FILE before importing, MUST READ:  goo.gl/2fZDQL
-                    
-// https://github.com/tazotodua/useful-php-scripts  
-if (isset($_POST['backup'])) {
-
-    function EXPORT_TABLES($host,$user,$pass,$name,       $tables=false, $backup_name=false){ 
-    set_time_limit(3000); $mysqli = new mysqli($host,$user,$pass,$name); $mysqli->select_db($name); $mysqli->query("SET NAMES 'utf8'");
-    $queryTables = $mysqli->query('SHOW TABLES'); while($row = $queryTables->fetch_row()) { $target_tables[] = $row[0]; }   if($tables !== false) { $target_tables = array_intersect( $target_tables, $tables); } 
-    $content = "SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";\r\nSET time_zone = \"+00:00\";\r\n\r\n\r\n/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n/*!40101 SET NAMES utf8 */;\r\n--\r\n-- Database: `".$name."`\r\n--\r\n\r\n\r\n";
-    foreach($target_tables as $table){
-        if (empty($table)){ continue; } 
-        $result = $mysqli->query('SELECT * FROM `'.$table.'`');     $fields_amount=$result->field_count;  $rows_num=$mysqli->affected_rows;     $res = $mysqli->query('SHOW CREATE TABLE '.$table); $TableMLine=$res->fetch_row(); 
-        $content .= "\n\n".$TableMLine[1].";\n\n";   $TableMLine[1]=str_ireplace('CREATE TABLE `','CREATE TABLE IF NOT EXISTS `',$TableMLine[1]);
-        for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0) {
-            while($row = $result->fetch_row())  { //when started (and every after 100 command cycle):
-                if ($st_counter%100 == 0 || $st_counter == 0 )  {$content .= "\nINSERT INTO ".$table." VALUES";}
-                    $content .= "\n(";    for($j=0; $j<$fields_amount; $j++){ $row[$j] = str_replace("\n","\\n", addslashes($row[$j]) ); if (isset($row[$j])){$content .= '"'.$row[$j].'"' ;}  else{$content .= '""';}     if ($j<($fields_amount-1)){$content.= ',';}   }        $content .=")";
-                //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler
-                if ( (($st_counter+1)%100==0 && $st_counter!=0) || $st_counter+1==$rows_num) {$content .= ";";} else {$content .= ",";} $st_counter=$st_counter+1;
-            }
-        } $content .="\n\n\n";
-    }
-    $content .= "\r\n\r\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;\r\n/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;\r\n/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;";
-    $backup_name = $backup_name ? $backup_name : $name.'___('.date('H-i-s').'_'.date('d-m-Y').').sql';
-    ob_get_clean(); header('Content-Type: application/octet-stream');  header("Content-Transfer-Encoding: Binary");  header('Content-Length: '. (function_exists('mb_strlen') ? mb_strlen($content, '8bit'): strlen($content)) );    header("Content-disposition: attachment; filename=\"".$backup_name."\""); 
-    echo $content; exit;
-    } 
-}
-     //see import.php too
-?>    
     <div class="container-fluid">
         <div class="row">
           <ol class="breadcrumb">
@@ -165,18 +113,72 @@ if (isset($_POST['backup'])) {
         </div>
         <div class="row">
            <div class="col-sm-4">
-            <label>Restore Database:</label>
-            <form action="" method="post" enctype="multipart/form-data">
-                Select image to upload:
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" class="btn btn-default" name="submit">
-            </form>
+                <div class="panel-body">
+                    <label><b class="fa fa-file-excel-o fa-bg"></b> Import CSV File for Customers</label>
+                  <form action="" method="post" enctype="multipart/form-data" id="importFrm">
+                        <input type="file" name="file" class="form-control">
+                        <input type="submit" class="btn btn-primary form-control" name="importSubmit" value="IMPORT">
+                    </form>
+                </div>
+                <?php
+                    if(isset($_POST['importSubmit'])){
+                        
+                        //validate whether uploaded file is a csv file
+                        $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+                        if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'],$csvMimes)){
+                            if(is_uploaded_file($_FILES['file']['tmp_name'])){
+                                //open uploaded csv file with read only mode
+                                $csvFile = fopen($_FILES['file']['tmp_name'], 'r');          
+                                //skip first line
+                                fgetcsv($csvFile);
+                                //parse data from csv file line by line
+                                while(($line = fgetcsv($csvFile)) !== FALSE){
+                                    //check whether member already exists in database with same email
+                                    $sql = mysqli_query($db,"SELECT * FROM tbl_customers WHERE tin = '".$line[1]."'");
+                                    $result = mysqli_fetch_assoc($sql);
+                                    if($result > 0){
+                                        //update member data
+                                        $query1 = mysqli_query($db,"UPDATE tbl_customers SET full_name = '".$line[0]."',  terms = '".$line[2]."', opidno = '".$line[3]."', bstyle = '".$line[4]."', address = '".$line[5]."' WHERE tin = '".$line[1]."'");
+                                    }else{
+                                        //insert member data into database
+                                        $query2 = mysqli_query($db,"INSERT INTO tbl_customers (full_name, tin, terms, opidno, bstyle, address) VALUES ('".$line[0]."','".$line[1]."','".$line[2]."','".$line[3]."','".$line[4]."','".$line[5]."')");
+                                    }
+                                }
+                                
+                                //close opened csv file
+                                fclose($csvFile);
+
+                                $qstring = '?status=succ';
+?>
+                                      <div class="alert alert-success alert-dismissable">
+                                          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                        <strong>Successfully Added!</strong>
+                                      </div>
+<?php                                
+                            }else{
+                                $qstring = '?status=err';
+?>
+                                      <div class="alert alert-warning alert-dismissable">
+                                          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                        <strong>Failed to Add!</strong> Try Again.
+                                      </div>
+<?php
+                            }
+                        }else{
+                            $qstring = '?status=invalid_file';
+?>
+                                      <div class="alert alert-danger alert-dismissable">
+                                          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                        <strong>Invalid CSV File!</strong> Try Again.
+                                      </div>
+<?php                            
+                        }
+                    }
+
+?>
            </div>
            <div class="col-sm-4">
-           <label>Backup Database:</label>
-               <form class="form-group" method="POST" action="">
-                   <button class="btn btn-primary" type="submit" name="backup"><b class="fa fa-database">&nbsp;</b>Backup</button>
-               </form>
+           
            </div>
            <div class="col-sm-4">
 
